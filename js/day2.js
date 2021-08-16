36b914f8ceb7ad3fdb880a2546e6d11d183ddca9
William <wbrandev@gmail.com>
2021-08-16 02:27:27 -0700

    commit of day-2

---
+++ js/day2.js
@@ -0,0 +1,10 @@
+const str = "Supercalifragilisticexpialidocious";
+const num = 15;
+const bignum = 1n;
+
+console.log(str.length);
+
+console.log(num);
+console.log(bignum);
+console.log(typeof num);
+console.log(typeof bignum);
